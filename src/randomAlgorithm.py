d56711eb17c4b3585a987017b180c63a25eb4eb7
David Mihalcenco <david.mihalcenco@gmail.com>
2023-11-15 18:51:24 +0200

    SCRUM-19 Implemented random_choice function with diferent functionality

---
+++ src/randomAlgorithm.py
@@ -0,0 +1,21 @@
+import random
+
+def random_choice(data, end=None, number=None):
+    """
+    data - It can be the beginning of a range or a list.\n
+    end - End of an interval. [Optional]\n
+    number - Number of random objects from list. [Optional]
+    """
+    if isinstance(data, int) and isinstance(end, int) and number is None:
+        return random.randint(data, end)
+    elif isinstance(data, list) and end is None and number is None:
+        return random.choice(data)
+    elif isinstance(data, list) and end is None and isinstance(number, int) :
+        randomlist = []
+        for _ in range(number):
+            algorithm = random.choice(data)
+            data.remove(algorithm)
+            randomlist.append(algorithm)
+        return randomlist
+    else:
+        return None
