7c846b173aabb73e8b4ff52320c421864745722a
Andrei Girnet <girnetandrei0@gmail.com>
2023-11-22 21:37:37 +0200

    [SCRUM-14] Connect all compotents for demo

--- src/functions.py
+++ src/functions.py
@@ -1,44 +1,44 @@
 from functools import reduce
 import numpy as np
 
-def min_leafs(*data):
+def min_leafs(data):
     """
     data - List of data.
     """
     return min(data)
 
-def max_leafs(*data):
+def max_leafs(data):
     """
     data - List of data.
     """
     return max(data)
 
-def arithmetic_average(*data):
+def arithmetic_average(data):
     """
     data - List of data.
     """
     return sum(data) / len(data)
 
-def geometric_average(*data):
+def geometric_average(data):
     """
     data - List of data.
     """
     return reduce(lambda x, y: x * y, data) ** (1 / len(data))
 
-def weighted_average(data, dataw):
-    """
-    data - List of data.\n
-    dataw - List of weights.
-    """
-    weighted_sum = sum(data[i] * dataw[i] for i in range(len(data)))
-    total_weights = sum(dataw)
-    return weighted_sum / total_weights
+# def weighted_average(data, dataw):
+#     """
+#     data - List of data.
+#     dataw - List of weights.
+#     """
+#     weighted_sum = sum(data[i] * dataw[i] for i in range(len(data)))
+#     total_weights = sum(dataw)
+#     return weighted_sum / total_weights
 
-def calculate_median(*data):
+def calculate_median(data):
     """
     data - List of data.
     """
-    sorted_list = sorted(*data)
+    sorted_list = sorted(data)
     length = len(sorted_list)
     
     if length % 2 == 0:
@@ -50,19 +50,19 @@ def calculate_median(*data):
     
     return median
 
-def power_limit(data, power):
-    """
-    data - List of data.
-    """
-    result = np.power(data, power)
-    return np.max(np.maximum(0, np.minimum(1, result)))
+# def power_limit(data, power):
+#     """
+#     data - List of data.
+#     """
+#     result = np.power(data, power)
+#     return np.max(np.maximum(0, np.minimum(1, result)))
 
-def log_limit(data):
-    """
-    data - List of data.
-    """
-    result = np.log(data + 1)
-    return np.max(np.maximum(0, np.minimum(1, result)))
+# def log_limit(data):
+#     """
+#     data - List of data.
+#     """
+#     result = np.log(data + 1)
+#     return np.max(np.maximum(0, np.minimum(1, result)))
 
 def sin_limit(data):
     """
--- src/randomTree.py
+++ src/randomTree.py
@@ -10,6 +10,7 @@ class RandomTree:
     def __init__(self, content):
         self.content = content
         self.children = []
+        self._last_level_ = []
 
     def add_child(self, tree):
         self.children.append(tree)
@@ -30,9 +31,19 @@ class RandomTree:
         for child in self.children:
             child.show(level + 1)
 
-    
+    def add_data_in_tree(self, data_line):
+        if self._last_level_ == []:
+            raise Exception("Non root node or tree was not generated random with generate_random_tree()")
+        # values_count = randomAlgorithm.random_choice(1, len(data_line))
+        
+        for node in self._last_level_:
+            # choosen_values = randomAlgorithm.random_choice(data=data_line, number=values_count)
+            for value in data_line:
+                node.add_child(RandomTree(value))
+        return
+
     @staticmethod
-    def generate_random_tree(data_line):
+    def generate_random_tree():
         """
         Generate a randomized tree from leaves up to the root.
         """
@@ -47,15 +58,14 @@ class RandomTree:
 
         nodes = []
         
-        values_count = randomAlgorithm.random_choice(1, len(data_line))
+        values_count = randomAlgorithm.random_choice(3, len(node_functions))
         # choose random amount of values to be leaf nodes
-        choosen_values = randomAlgorithm.random_choice(data=data_line, number=values_count)
+        choosen_values = randomAlgorithm.random_choice(data=node_functions, number=values_count)
 
-        tmp = []
+        last_level_tmp = []
         for value in choosen_values:
-            tmp.append(RandomTree(value))
-        
-        nodes.append(tmp)
+            last_level_tmp.append(RandomTree(value))
+        nodes.append(last_level_tmp)
 
         # get a random number of levels which the result tree will be made of
         # (leaves and root count as levels)
@@ -80,9 +90,52 @@ class RandomTree:
             nodes.append(tmp)
         
         # set the root node
-        output_node = RandomTree("OUTPUT")
-
+        output_node = RandomTree(randomAlgorithm.random_choice(node_functions))
+        output_node._last_level_.extend(last_level_tmp)
         for node in nodes[-1]:
             output_node.add_child(node)
 
         return output_node
+
+    @staticmethod
+    def __execute_function__(name: str, data: list):
+        """Find functioon by name and execute it with data
+
+        Args:
+            name (str): function name
+            data (list): function argv
+
+        Raises:
+            Exception: Function not find
+
+        Returns:
+            float: return function's return
+        """
+        if name == "min_leafs":
+            return functions.min_leafs(data)
+        elif name == "max_leafs":
+            return functions.max_leafs(data)
+        elif name == "arithmetic_average":
+            return functions.arithmetic_average(data)
+        elif name == "geometric_average":
+            return functions.geometric_average(data)
+        elif name == "calculate_median":
+            return functions.calculate_median(data)
+        elif name == "log_limit":
+            return functions.log_limit(data)
+        elif name == "sin_limit":
+            return functions.sin_limit(data)
+        else:
+            raise Exception("Function not find [" + name + "]")
+
+    def get_tree_result(self) -> float:
+        if isinstance(self.content, float):
+            return self.content
+
+        values = []
+        node:RandomTree
+        for node in self.children:
+            values.append(node.get_tree_result())
+        if values == []:
+            raise Exception("Bad Tree")
+        return RandomTree.__execute_function__(self.content, values)
